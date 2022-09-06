cea67e54c123caba6aacae6b5608bfb2b365fa2b
3 <demo3@gmail.com>
2022-09-06 18:37:17 -0400

    first commit

---
+++ proj1/src/com/kb/beens/utility.java
@@ -0,0 +1,16 @@
+package com.kb.beens;
+
+import java.util.Date;
+
+public class utility {
+	
+	public Date getSysDate() {
+		return new Date();
+	}
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
